d44b9300dfbfd144a9fde8199665d004c759ccfc
Henrique Pacheco <henrique.pacheco@vizzuality.com>
2021-02-23 11:14:12 +0000

    Add logger

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const MongoClient = require('mongodb').MongoClient;
 const config = require('config');
+const log = require('loglevel');
 
 async function main() {
     let client;
     try{
+        log.setLevel(config.get('log.level'));
+        log.info('Establishing connection with MongoDB...');
         client = await MongoClient.connect(config.get('mongo.uri'), {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
+
         const result = await client.db('control-tower').collection('users').countDocuments();
         console.log(result);
     } catch(err) {
