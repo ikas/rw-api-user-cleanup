0c91a5bc96120687c911f02ac10ac2e31439104e
Henrique Pacheco <henrique.pacheco@vizzuality.com>
2021-02-23 11:51:53 +0000

    Removing users with provider google or local without email

--- index.js
+++ index.js
@@ -2,6 +2,24 @@ const MongoClient = require('mongodb').MongoClient;
 const config = require('config');
 const log = require('loglevel');
 
+const deleteWithGoogleProvider = async (client) => {
+    log.info('Preparing to delete users with provider "google-plus"...');
+    const countRes = await client
+        .db('control-tower')
+        .collection('users')
+        .deleteMany({ provider: 'google-plus' });
+    log.info(`Deleted ${countRes.result.n} users with provider "google-plus"`);
+};
+
+const deletedWithLocalProvider = async (client) => {
+    log.info('Preparing to delete users with provider "local" without email...');
+    const countRes = await client
+        .db('control-tower')
+        .collection('users')
+        .deleteMany({ provider: 'local', email: { $eq: null } });
+    log.info(`Deleted ${countRes.result.n} users with provider "local" without email`);
+};
+
 async function main() {
     let client;
     try{
@@ -12,8 +30,11 @@ async function main() {
             useUnifiedTopology: true,
         });
 
-        const result = await client.db('control-tower').collection('users').countDocuments();
-        console.log(result);
+        const initialCountUsers = await client.db('control-tower').collection('users').countDocuments();
+        log.info(`Total number of users before running scripts: ${initialCountUsers}`);
+
+        await deleteWithGoogleProvider(client);
+        await deletedWithLocalProvider(client);
     } catch(err) {
         console.error(err);
     } finally{
@@ -21,4 +42,6 @@ async function main() {
     }
 }
 
-main().then(() => console.log('script ended')).catch(err => console.error(err));
+main()
+    .then(() => console.log('script ended'))
+    .catch(err => console.error(err));
