2bb2d0374fb462200434e49b7b956154e1e0bb1f
Henrique Pacheco <henrique.pacheco@vizzuality.com>
2021-03-12 09:30:55 +0000

    Remove logic for counting content owned by duplicate users

--- analysis.js
+++ analysis.js
@@ -1,4 +1,3 @@
-const axios = require('axios');
 const config = require('config');
 const log = require('loglevel');
 const fs = require('fs');
@@ -35,47 +34,6 @@ const fetchPageOfUsers = async (client, page) => {
 
     await usersPage.forEach(doc => users.push(doc));
     return users;
-}
-
-const countDatasets = async (client, id) => {
-    return client.db('dataset').collection('datasets').countDocuments({ userId: id });
-}
-
-const countLayers = async (client, id) => {
-    return client.db('layer').collection('layers').countDocuments({ userId: id });
-}
-
-const countWidgets = async (client, id) => {
-    return client.db('widget').collection('widgets').countDocuments({ userId: id });
-}
-
-const countSubscriptions = async (client, id) => {
-    return client.db('subscription').collection('subscriptions').countDocuments({ userId: id });
-}
-
-const countFWTeams = async (client, id) => {
-    return client.db('teams').collection('teams').countDocuments({ confirmedUsers: { $elemMatch: { id } } });
-}
-
-const countVocabulary = async (client, id) => {
-    return client.db('vocabulary').collection('vocabularies').countDocuments({ userId: id });
-}
-
-const countAreas = async (client, id) => {
-    return client.db('area').collection('areas').countDocuments({ userId: id });
-}
-
-const countMetadata = async (client, id) => {
-    return client.db('metadata').collection('metadatas').countDocuments({ userId: id });
-}
-
-const countDashboards = async (id) => {
-    const res = await axios.get(
-        `${config.get('api.url')}/v1/dashboard?user=${id}`,
-        { headers: { Authorization: `Bearer ${config.get('api.token')}` }}
-    );
-
-    return res.data.meta['total-items'];
 }
 
 const main = async () => {
@@ -125,22 +83,6 @@ const main = async () => {
         );
 
         let allDuplicateEmails = usersWithEmail.filter(user => duplicateEmails.includes(user.email));
-        let it = 1;
-        for (let user of allDuplicateEmails) {
-            user.datasets = await countDatasets(client, user.id);
-            user.layers = await countLayers(client, user.id)
-            user.widgets = await countWidgets(client, user.id);
-            user.dashboards = await countDashboards(user.id);
-            user.subscriptions = await countSubscriptions(client, user.id);
-            user.fwTeams = await countFWTeams(client, user.id);
-            user.vocabularies = await countVocabulary(client, user.id);
-            user.areas = await countAreas(client, user.id);
-            user.metadata = await countMetadata(client, user.id);
-
-            log.info(`Processed content for duplicate user ${it}/${allDuplicateEmails.length}`);
-            it++;
-        }
-
         allDuplicateEmails.forEach(dup => duplicatesFile.write(`${Object.values(dup).join(',')}\n`));
         duplicatesFile.end();
 
